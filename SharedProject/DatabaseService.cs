7d61cb21e5b8cf688b69023978d74dcdabf4a975
Erik Král <ekral@utb.cz>
2022-10-17 08:35:16 +0200

    EnsureCreated fix

--- SharedProject/DatabaseService.cs
+++ SharedProject/DatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,39 @@ namespace SharedProject
         private const string path = "database.db";
         private readonly string connectionString;
 
-       
+        public DatabaseService()
+        {
+            SqliteConnectionStringBuilder builder = new SqliteConnectionStringBuilder();
+            builder.DataSource = path;
+
+            connectionString = builder.ToString();
+        }
 
         /// <summary>
         /// Verifies that the database exists. And if it does not exist, it creates a new database.
         /// </summary>
-        public async Task EnsureCreatedAsync()
+        public async Task<bool> EnsureCreatedAsync()
         {
-            
+            if (File.Exists(path)) return false;
+
+            await using SqliteConnection connection = new SqliteConnection(connectionString);
+            await connection.OpenAsync();
+
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE Mortgage 
+                (
+                    Id INTEGER PRIMARY KEY, 
+                    LoanAmount DOUBLE,
+                    InterestRate DOUBLE,
+                    LoanTerm INTEGER
+                )
+            ";
+
+            command.ExecuteNonQuery();
+
+            return true;
         }
 
         public async Task<List<Model>> GetAll()
