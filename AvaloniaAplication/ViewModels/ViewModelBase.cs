43b73e5e1558c1df7b5d9820bccaf8927ae4eade
Erik Král <ekral@utb.cz>
2022-10-25 13:49:14 +0200

    ViewModelBase complet

--- AvaloniaAplication/ViewModels/ViewModelBase.cs
+++ AvaloniaAplication/ViewModels/ViewModelBase.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AvaloniaAplication.ViewModel
+namespace AvaloniaAplication.ViewModels
 {
-    internal class ViewModelBase
+    public class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
+        {
+            ArgumentNullException.ThrowIfNull(propertyName);
+
+            if(EqualityComparer<T>.Default.Equals(field, newValue)) return false;
+
+            field = newValue;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            NotifyPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        public bool SetPropery<T, TModel>(T oldValue, T newValue, TModel model, Action<TModel, T> action, [CallerMemberName] string? propertyName = null)
+        {
+            ArgumentNullException.ThrowIfNull(model);
+            ArgumentNullException.ThrowIfNull(propertyName);
+
+            if (EqualityComparer<T>.Default.Equals(oldValue, newValue)) return false;
+
+            action.Invoke(model, newValue);
+
+            NotifyPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            ArgumentNullException.ThrowIfNull(propertyName);
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
